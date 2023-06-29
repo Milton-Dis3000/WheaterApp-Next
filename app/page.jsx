c27733975fa07da89aa0d0e9efa53dcc0cfd3a7e
Milthon <menteprospera3000@gmail.com>
2023-06-29 15:19:33 -0500

    SecondComitt

--- app/page.jsx
+++ app/page.jsx
@@ -3,93 +3,47 @@ import styles from './page.module.css'
 
 export default function Home() {
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>app/page.js</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+ <div className='principalContainer'>
+  <div className='firstContainer'>
+    <div className='searchContainer'>
+      <div className='searchGroup'>
+        <button className='buttonSearch'>Seach for places</button>
+        <div id='containerLocation'>
+        <img id='mylocation' src="/mylocation.svg" alt="" />
         </div>
+        
+      </div>
+      
+    </div>
+    <div className='climeImage'>
+      <img src="" alt="cloudBackground" />
+      <img src="" alt="shower" />
+    </div>
+    <div className='boxshowerText'>
+      <div>
+        <h2>15°C</h2>
+      </div>
+      <div>
+        <h3>Shower</h3>
+      </div>
+      <div>
+        <h4>Today</h4>
+        <h4>Fri.5 Jun</h4>
+      </div>
+      <div>
+        <img src="" alt="icono" />
+        <h4>Helsinki</h4>
+
       </div>
 
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+    </div>
 
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
+  </div>
+  <div className='secondContainer'>
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
+  </div>
 
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore the Next.js 13 playground.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+ </div>
+ 
   )
 }
