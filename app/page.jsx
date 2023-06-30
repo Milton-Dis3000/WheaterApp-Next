53d7a35faef63caacf2973d080fe0281df0c4e34
Milthon <menteprospera3000@gmail.com>
2023-06-30 16:56:42 -0500

    SecondCommit

--- app/page.jsx
+++ app/page.jsx
@@ -16,9 +16,23 @@ function IconLocation() {
   );
 }
 
+function IconWsw() {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="16"
+      height="16"
+      fill="currentColor"
+      className="bi bi-cursor-fill"
+      viewBox="0 0 16 16"
+    >
+      <path d="M14.082 2.182a.5.5 0 0 1 .103.557L8.528 15.467a.5.5 0 0 1-.917-.007L5.57 10.694.803 8.652a.5.5 0 0 1-.006-.916l12.728-5.657a.5.5 0 0 1 .556.103z" />
+    </svg>
+  );
+}
+
 export default function Home() {
   return (
-    
     <div className="principalContainer">
       <div className="firstContainer">
         <div className="searchContainer">
@@ -49,40 +63,29 @@ export default function Home() {
             <p>Fri,5 Jun</p>
           </div>
           <div className="footerIconText">
-            <IconLocation/>
+            <IconLocation />
             <p>Helsinki</p>
           </div>
         </div>
       </div>
-
 
       <div className="secondContainer">
         <div className="days">
           <div className="day1">
             <p>Tomorrow</p>
             <div className="container-Cloud">
-            <img id="cloud-img" src="/Sleet.png" alt="" />
+              <img id="cloud-img" src="/Sleet.png" alt="" />
             </div>
-            
+
             <div className="max-min-grad-text">
-            <p>16°C</p>
-            <p>11°C</p>
+              <p>16°C</p>
+              <p>11°C</p>
             </div>
-           
           </div>
-          <div className="day2">
-
-          </div>
-          <div className="day3">
-
-          </div>
-          <div className="day4">
-
-          </div>
-          <div className="day5"> 
-
-          </div>
-          
+          <div className="day2"></div>
+          <div className="day3"></div>
+          <div className="day4"></div>
+          <div className="day5"></div>
         </div>
         <div className="hightlightsText">
           <h2>Today’s Hightlights</h2>
@@ -91,18 +94,12 @@ export default function Home() {
           <div className="temp1">
             <p>Wind status</p>
             <h2>7 mph</h2>
-            <img src="" alt="Icons" />
-
+            <IconWsw/>
           </div>
-          <div className="temp2">
-          </div>
-          <div className="temp3">
-          </div>
-          <div className="temp4">
-          </div>
-
+          <div className="temp2"></div>
+          <div className="temp3"></div>
+          <div className="temp4"></div>
         </div>
-
       </div>
     </div>
   );
