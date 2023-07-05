8a2b432700adbaa19da3549505520781b6d3dcde
Milthon <menteprospera3000@gmail.com>
2023-07-05 00:12:20 -0500

    NewCommitFinish

--- app/page.jsx
+++ app/page.jsx
@@ -35,98 +35,126 @@ function IconWsw() {
   );
 }
 
+
+
+// Función para obtener el día de la semana abreviado
+function getDayOfWeek(dateString) {
+  const daysOfWeek = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+  const date = new Date(dateString);
+  const dayOfWeek = date.getDay();
+  return daysOfWeek[dayOfWeek];
+}
+
+
+
 function Home() {
+
+  function getFormattedDate(dateString) {
+    const date = new Date(dateString);
+    const dayOfWeek = getDayOfWeek(dateString);
+    const day = date.getDate();
+    const month = date.toLocaleDateString('default', { month: 'short' });
+    return `${dayOfWeek}, ${day} ${month}`;
+  }
+
+
+
   const [data, setData] = useState({
     celcius: 10,
     name: "Helsinki",
     humidity: 84,
     speed: 7,
-    visibility: 6.4, 
+    visibility: 6.4,
     pressure: 998,
-    image: 'Shower.png',
-    weatherText: 'Shower',
-
+    image: "Shower.png",
+    weatherText: "Shower",
   });
- 
 
-const[name, setName] = useState('');
-const [error, setError] = useState('');
-
-// ..........CONSTANTE FORECAST...............................//
-const [forecast, setForecast] = useState([]);
-
-
-
+  const [name, setName] = useState("");
+  const [error, setError] = useState("");
+  const [forecast, setForecast] = useState([]);
 
   // search condition
-  const handleClick = () =>{
-    if (name !== ""){
-      const apiUrl =
-      `https://api.openweathermap.org/data/2.5/weather?q=${name}&appid=cc594a74503770cb5ddca26ecd57daa7&units=metric`;
-    axios
-      .get(apiUrl)
-      .then((res) => {
-        let imagePath='';
-        let weatherText = '';
+  const handleClick = () => {
+    if (name !== "") {
+      const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${name}&appid=cc594a74503770cb5ddca26ecd57daa7&units=metric`;
+      axios
+        .get(apiUrl)
+        .then((res) => {
+          let imagePath = "";
+          let weatherText = "";
 
+          if (res.data.weather[0].main == "Clouds") {
+            imagePath = "/HeavyCloud.png";
+            weatherText = "Clouds";
+          } else if (res.data.weather[0].main == "Clear") {
+            imagePath = "/Clear.png";
+            weatherText = "Clear";
+          } else if (res.data.weather[0].main == "Rain") {
+            imagePath = "/Sleet.png";
+            weatherText = "Rain";
+          } else if (res.data.weather[0].main == "Drizzle") {
+            imagePath = "/HeavyRain.png";
+            weatherText = "Drizzle";
+          } else if (res.data.weather[0].main == "Mist") {
+            imagePath = "/LightCloud.png";
+            weatherText = "Mist";
+          } else {
+            imagePath = "/Shower.png";
+            weatherText = "Shower";
+          }
 
-        if (res.data.weather[0].main == "Clouds"){
-          imagePath = "/HeavyCloud.png"
-          weatherText = "Clouds"
-        } else if (res.data.weather[0].main == "Clear"){
-          imagePath = "/Clear.png"
-          weatherText = "Clear"
-        } else if (res.data.weather[0].main == "Rain"){
-          imagePath = "/Sleet.png"
-          weatherText = "Rain"
-        
-        } else if (res.data.weather[0].main == "Drizzle"){
-          imagePath = "/HeavyRain.png"
-          weatherText = "Drizzle"
-        } else if (res.data.weather[0].main == "Mist"){
-          imagePath = "/LightCloud.png"
-          weatherText = "Mist"
-        } else {
-          imagePath = '/Shower.png'
-          weatherText = "Shower"
-          
-        }
-      
-          
-        console.log(res.data);
-        setData({
-          ...data,
-          celcius: res.data.main.temp,
-          name: res.data.name,
-          humidity: res.data.main.humidity,
-          speed: res.data.wind.speed,
-          visibility: res.data.visibility,
-          pressure: res.data.main.pressure,
-          image: imagePath,
-          weatherText: weatherText,
-          
+          console.log(res.data);
+          setData({
+            ...data,
+            celcius: res.data.main.temp,
+            name: res.data.name,
+            humidity: res.data.main.humidity,
+            speed: res.data.wind.speed,
+            visibility: res.data.visibility,
+            pressure: res.data.main.pressure,
+            image: imagePath,
+            weatherText: weatherText,
+          });
+          setError("");
         })
-        setError('');
-      })
-      .catch(err => {
-        if (err.response.status == 404){
-          setError("Invalid City Name")
-        } else {
-          setError('');
-        }
+        .catch((err) => {
+          if (err.response.status == 404) {
+            setError("Invalid City Name");
+          } else {
+            setError("");
+          }
 
-          console.log(err)
+          console.log(err);
         });
     }
-  }
+  };
 
+  useEffect(() => {
+    if (name !== "") {
+      const apiUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${name}&appid=cc594a74503770cb5ddca26ecd57daa7&units=metric`;
 
+      axios
+        .get(apiUrl)
+        .then((res) => {
+          setForecast(res.data.list.slice(1, 6)); // Obtiene los datos para los próximos 5 días (excluyendo el día actual)
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    }
+  }, [name]);
 
   return (
     <div className="principalContainer">
       <div className="searchFond">
         <div className="search">
-          <input type="text" placeholder="search location" onChange={e => setName(e.target.value)} />
+          <input
+            id="searchStyle"
+            type="text"
+            placeholder="search location"
+            onChange={(e) => setName(e.target.value)}
+          />
           <button id="buttonSearch" onClick={handleClick}>
             <h4>Search</h4>
           </button>
@@ -134,7 +162,6 @@ const [forecast, setForecast] = useState([]);
         <div className="error">
           <p>{error}</p>
         </div>
-
       </div>
 
       <div className="firstContainer">
@@ -152,7 +179,7 @@ const [forecast, setForecast] = useState([]);
             src="/Cloud-background.png"
             alt="cloudBackground"
           />
-          <img id="shower" src= {data.image} alt="shower" />
+          <img id="shower" src={data.image} alt="shower" />
         </div>
         <div className="boxshowerText">
           <div className="centigradsText">
@@ -172,22 +199,23 @@ const [forecast, setForecast] = useState([]);
         </div>
       </div>
 
+    
       <div className="secondContainer">
         <div className="days">
-
-          <div className="day1">
-            <p>Tomorrow</p>
+        {forecast.map((day, index) => (
+          <div key={index} className={`day${index + 1}`}>
+            <p>{getFormattedDate(day.dt_txt)}</p>
             <div className="container-Cloud">
               <img id="cloud-img" src="/Sleet.png" alt="" />
             </div>
-
             <div className="max-min-grad-text">
-              <p>16°C</p>
-              <p>11°C</p>
+              <p>{day.main.temp_max}°C</p>
+              <p>{day.main.temp_min}°C</p>
             </div>
           </div>
+        ))}
 
-          <div className="day2">
+          {/* <div className="day2">
             <p>Wednesday</p>
             <div className="container-Cloud">
               <img id="cloud-img" src="/Sleet.png" alt="" />
@@ -235,9 +263,8 @@ const [forecast, setForecast] = useState([]);
               <p>16°C</p>
               <p>11°C</p>
             </div>
-          </div>
-         
-        </div>
+          </div> */}
+        </div> 
 
         <div className="hightlightsText">
           <h2>Today’s Hightlights</h2>
@@ -266,25 +293,23 @@ const [forecast, setForecast] = useState([]);
           </div>
 
           <div className="temp3">
-          <p>Visibility</p>
+            <p>Visibility</p>
             <div className="windText-Central">
               <h2>{data.visibility}</h2>
               <h4>miles</h4>
             </div>
-            
           </div>
-          
+
           <div className="temp4">
-          <p>Air Pressure</p>
+            <p>Air Pressure</p>
             <div className="windText-Central">
               <h2>{data.pressure}</h2>
               <h4>mb</h4>
             </div>
           </div>
-
         </div>
       </div>
     </div>
   );
 }
-export default Home
+export default Home;
